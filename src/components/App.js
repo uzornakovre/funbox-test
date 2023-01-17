bd841316c090b322dffbdb629fbbc11635d035a7
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-01-17 17:22:05 +0300

    feat: markup

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="page__content">
+      <header className="header">
+        <h1 className="header__title">Ты сегодня покормил кота?</h1>
+      </header>
+      <main className="cards">
+        <ul className="cards__list">
+          <li className="cards__list-item">
+            <article className="card">
+              <div className="card__content">
+                <p className="card__overhead">Сказочное заморское яство</p>
+                <h2 className="card__title">Нямушка</h2>
+                <p className="card__subtitle">с фуа-гра</p>
+                <p class="card__description"></p>
+              </div>
+              <p className="card__caption"></p>
+            </article>
+          </li>
+          <li className="cards__list-item">
+
+          </li>
+          <li className="cards__list-item">
+
+</li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
