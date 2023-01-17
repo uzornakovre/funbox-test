a0e0365365edb5669fd3c327af8d0776440d8aae
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-01-17 18:59:05 +0300

    feat: cards

--- src/components/App.js
+++ src/components/App.js
@@ -14,14 +14,56 @@ function App() {
                 <p className="card__overhead">Сказочное заморское яство</p>
                 <h2 className="card__title">Нямушка</h2>
                 <p className="card__subtitle">с фуа-гра</p>
-                <p class="card__description"></p>
+                <p className="card__description"><span className="bold">10 </span>порций</p>
+                <p className="card__description">мышь в подарок</p>
+                <div className="card__weight">
+                  <p className="card__weight-value">0,5</p>
+                  <p className="card__weight-unit">кг</p>
+                </div>
               </div>
-              <p className="card__caption"></p>
+              <p className="card__caption">Чего сидишь? Порадуй котэ,&ensp;
+                <a className="card__caption-link" href="#" target="_blank">купи</a>.
+              </p>
             </article>
           </li>
           <li className="cards__list-item">
-
+            <article className="card">
+              <div className="card__content">
+                <p className="card__overhead">Сказочное заморское яство</p>
+                <h2 className="card__title">Нямушка</h2>
+                <p className="card__subtitle">с рыбой</p>
+                <p className="card__description"><span className="bold">40 </span>порций</p>
+                <p className="card__description"><span className="bold">2 </span>мыши в подарок</p>
+                <div className="card__weight">
+                  <p className="card__weight-value">2</p>
+                  <p className="card__weight-unit">кг</p>
+                </div>
+              </div>
+              <p className="card__caption">Чего сидишь? Порадуй котэ,&ensp;
+                <a className="card__caption-link" href="#" target="_blank">купи</a>.
+              </p>
+            </article>
           </li>
+            <article className="card">
+              <div className="card__content">
+                <p className="card__overhead">Сказочное заморское яство</p>
+                <h2 className="card__title">Нямушка</h2>
+                <p className="card__subtitle">с курой</p>
+                <p className="card__description"><span className="bold">100 </span>порций</p>
+                <p className="card__description"><span className="bold">5 </span> мышей в подарок</p>
+                <p className="card__description">заказчик доволен</p>
+                <div className="card__weight">
+                  <p className="card__weight-value">5</p>
+                  <p className="card__weight-unit">кг</p>
+                </div>
+                {/* <div className="card__corner">
+                  kjhkjhkjh
+                </div> */}
+              </div>
+              <p className="card__caption">Чего сидишь? Порадуй котэ,&ensp;
+                <a className="card__caption-link" href="#" target="_blank">купи</a>.
+              </p>
+            </article>
           <li className="cards__list-item">
 
 </li>
