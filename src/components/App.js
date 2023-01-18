a6da3beb66920aafa9d62fb6f4efe9466aa70abf
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-01-18 04:00:25 +0300

    fix: hotfixes

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,6 @@ import dishData from '../utils/dishData';
 
 function App() {
 
-  // const [selectedCards, setSelectedCards] = React.useState([]);
-  
-  // function handleCardClick(card) {
-  //   if (selectedCards.some(item => item === card)) {
-  //     setSelectedCards(selectedCards.filter(item => item !== card));
-  //   } else {
-  //     setSelectedCards([card, ...selectedCards]);
-  //   }
-  // }
-
   return (
     <div className="page__content">
       <header className="header">
@@ -24,22 +14,16 @@ function App() {
           <li className="cards__list-item">
             <Card dish={dishData.foieGras}
                   inStock={true}
-                  // selectCard={handleCardClick}
-                  // selectedCards={selectedCards}
              />
           </li>
           <li className="cards__list-item">
             <Card dish={dishData.fish}
                   inStock={true}
-                  // selectCard={handleCardClick}
-                  // selectedCards={selectedCards}
             /> 
           </li>
           <li className="cards__list-item">
             <Card dish={dishData.chicken}
                   inStock={false}
-                  // selectCard={handleCardClick}
-                  // selectedCards={selectedCards}
             />
           </li>
         </ul>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,48 @@
 import React from 'react';
-// import dishData from '../utils/dishData';
 
-function Card({ dish, inStock, selectCard, selectedCards }) {
+function Card({ dish, inStock }) {
 
-  const [isSelected, setSelected] = React.useState(false);
+  const [isSelected,   setSelected]     = React.useState(false);
+  const [overheadText, setOverheadText] = React.useState('Сказочное заморское яство');
 
   function handleCardClick() {
     if (inStock) {
-      // selectCard(dish);
       setSelected(!isSelected);
     }
   }
+
+  // function handleMouseOut(evt) {
+  //   if (isSelected) {
+  //     setOverheadText('Котэ не одобряет?');
+  //   } else {
+  //     setOverheadText('Сказочное заморское яство');
+  //   }
+  // }
+
+  // React.useState(() => {
+  //   if (isSelected) {
+  //     setOverheadText('Котэ не одобряет?');
+  //   } else {
+  //     setOverheadText('Сказочное заморское яство');
+  //   }
+  // }, [isSelected])
 
   return (
     <article className="card">
       <div className={`card__content ${!inStock && 
                       'card__content_disabled'} ${isSelected && 
                       'card__content_selected'}`}
-           onClick={handleCardClick}>
+           onClick={handleCardClick}
+           onMouseOut={handleMouseOut}>
+        
+        { isSelected && <p className={`card__overhead ${!inStock && 
+                                      'card__overhead_disabled'}
+                                       card__overhead_selected`}>{overheadText}</p>
+        }
 
-        <p className={`card__overhead ${!inStock && 
-                      'card__overhead_disabled'} ${isSelected && 
-                      'card__overhead_selected'}`}>Сказочное заморское яство</p>
+        { !isSelected && <p className={`card__overhead ${!inStock && 
+                                       'card__overhead_disabled'}`}>{overheadText}</p>
+        }
 
         <h2 className={`card__title ${!inStock && 
                       'card__title_disabled'}`}>Нямушка</h2>
@@ -52,11 +73,18 @@ function Card({ dish, inStock, selectCard, selectedCards }) {
                         'card__image_disabled'}`}></div>
 
       </div>
-      {inStock && <p className="card__caption">Чего сидишь? Порадуй котэ,&ensp;
-                    <button className="card__caption-link" type='button'>купи.</button>
+
+      { inStock && !isSelected && <p className="card__caption">Чего сидишь? Порадуй котэ,&ensp;
+                    <button className="card__caption-link" 
+                            type='button'
+                            onClick={handleCardClick}>купи.</button>
                   </p>
       }
-      {!inStock && <p className={`card__caption card__caption_disabled`}>
+
+      { inStock && isSelected && <p className="card__caption">{dish.description}</p>
+      }
+
+      { !inStock && <p className={`card__caption card__caption_disabled`}>
                       {`Печалька, ${dish.withWhat} закончился.`}
                    </p>
       } 
